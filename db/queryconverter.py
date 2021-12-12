62da3af07f093751c94c103244b492c31349d0d3
Jere <jereritchie@gmail.com>
2021-12-12 13:08:37 -0500

    filter during convert

--- db/queryconverter.py
+++ db/queryconverter.py
@@ -1,14 +1,23 @@
 """queryconverter.py is a set of utilities to convert queries from a sqlite database into a target object"""
+# standard modules
+from typing import List
 
-def to_obj_array(query_resp: list, obj_keys: list) -> list:
+def to_obj_array(query_resp: list, obj_keys: List[str], key_filter: List[str]=[]) -> list:
     """
     Converts a list of lists to a list of dictionaries.
     """
     out_list = []
+    if query_resp == []:
+        return out_list
     for in_record in query_resp:
         out_record = {}
         for i, k in enumerate(obj_keys):
             out_record[k] = in_record[i]
+        # use key_filter to subset specific values for output
+        if key_filter != []:
+            out_record = {
+                k:v for k,v in out_record.items() if k in key_filter
+            }
         out_list.append(out_record)
     return out_list
 
